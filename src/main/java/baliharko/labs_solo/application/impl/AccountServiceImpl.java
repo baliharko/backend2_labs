da4528d5c7e0810caf302d27fea2230de4788fbe
baliharko <bali@fauxdelorean.com>
2021-09-16 10:34:49 +0200

    moved frontend to static folder

--- src/main/java/baliharko/labs_solo/application/impl/AccountServiceImpl.java
+++ src/main/java/baliharko/labs_solo/application/impl/AccountServiceImpl.java
@@ -5,17 +5,20 @@ import baliharko.labs_solo.domain.Account;
 import baliharko.labs_solo.domain.IRiskAssessment;
 import baliharko.labs_solo.persistence.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
+@Slf4j
 @RequiredArgsConstructor
 public class AccountServiceImpl implements IAccountService {
 
     private final AccountRepository repository;
     private final IRiskAssessment riskClient;
-
 
     @Override
     public List<Account> getAllAccounts() {
@@ -33,6 +36,7 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public Account deposit(String holder, double amount) {
+        log.info("Depositing {} into account with holder: {}", amount, holder);
         Account acc = getAccountByHolder(holder);
         acc.deposit(amount);
         return acc;
@@ -40,6 +44,7 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public Account withdraw(String holder, double amount) {
+        log.info("Withdrawing {} from account with holder: {}", amount, holder);
         Account acc = getAccountByHolder(holder);
         acc.withdraw(amount);
         return acc;
@@ -48,12 +53,15 @@ public class AccountServiceImpl implements IAccountService {
     @Override
     public Account openAccount(String holder) {
 
+        log.info("Attempting to open account for {}", holder);
+
         if (repository.existsAccountByHolder(holder))
             throw new IllegalArgumentException("Account with holder name {" + holder + "} already exists.");
 
         if(!riskClient.passingCreditCheck(holder))
             throw new RuntimeException("Failed credit check for holder: " + holder);
 
+        log.info("Successfully opened account for {}", holder);
         return repository.save(new Account(holder, 0.0));
     }
 }
