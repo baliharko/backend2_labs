d3b1e672271c94f7784c278b4df2b05acc5d73c7
baliharko <bali@fauxdelorean.com>
2021-09-21 16:36:12 +0200

    docker risk working

--- src/main/java/baliharko/labs_solo/config/ApplicationConfiguration.java
+++ src/main/java/baliharko/labs_solo/config/ApplicationConfiguration.java
@@ -1,11 +1,13 @@
 package baliharko.labs_solo.config;
 
 import baliharko.labs_solo.risk.RiskAssessmentClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Configuration
 public class ApplicationConfiguration {
 
